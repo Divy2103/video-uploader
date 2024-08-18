a31c62eb407b35b5ce9f9eb3d6a4ee6972b9eb7f
divykaila <kailadivy098@gmail.com>
2024-08-18 16:54:48 +0530

    initialize and set-up project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
+
 
 const config: Config = {
   content: [
@@ -15,6 +17,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: [ "dark" ],
+  }
 };
 export default config;
